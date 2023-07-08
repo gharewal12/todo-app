fd3edc21f089f9003dbfee8e62d3ab7ac7aa435b
Granth Gharewal <gharewal12@gmail.com>
2023-07-08 20:35:22 +0530

    Initial setup and dark mode functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import BgWhite from "./images/bg-desktop-light.jpg";
+import BgDark from "./images/bg-desktop-dark.jpg";
+import { ReactComponent as Moon } from "./images/icon-moon.svg";
+import { ReactComponent as Sun } from "./images/icon-sun.svg";
+import { Typography } from "@mui/material";
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './theme/MainTheme';
 import './App.css';
 
+export const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
+
 function App() {
+  const [mode, setMode] = React.useState<"light" | "dark">("dark");
+
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+      },
+    }),
+    []
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme(mode)}>
+        <div className="App">
+          <CssBaseline />
+          <Container maxWidth={false} disableGutters>
+            <Box
+              sx={{
+                bgcolor: "#cfe8fc",
+                height: "40vh",
+                display: "flex",
+                width: "100%",
+                alignItems: "center",
+                justifyContent: "center",
+                backgroundImage: `url(${mode === "light" ? BgWhite : BgDark})`,
+              }}
+            >
+              <Grid container justifyContent={"center"}>
+                <Grid item>
+                  <Typography component='div' variant='h4'>TODO</Typography>
+                </Grid>
+                <Grid item>
+                  <IconButton
+                    sx={{ ml: 1 }}
+                    onClick={colorMode.toggleColorMode}
+                    color="inherit"
+                  >
+                    {mode === "dark" && <Sun />}
+                    {mode === "light" && <Moon />}
+                  </IconButton>
+                </Grid>
+              </Grid>
+            </Box>
+          </Container>
+        </div >
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ src/theme/MainTheme.ts
@@ -0,0 +1,51 @@
+import { createTheme } from "@mui/material";
+
+export const theme = (mode: any) => createTheme({
+    palette: {
+        mode,
+        ...(mode === 'light'
+            ? {
+                // palette values for light mode
+                primary: {
+                    main: "#fff",
+                },
+                divider: 'rgba(0, 0, 0, 0.12)',
+                background: {
+                    default: '#fff',
+                    paper: '#fff',
+                },
+                text: {
+                    primary: '#fff',
+                    secondary: 'rgba(0, 0, 0, 0.6)',
+                    disabled: 'rgba(0, 0, 0, 0.38)',
+                },
+                action: {
+                    active: 'rgba(0, 0, 0, 0.54)',
+                    hover: 'rgba(0, 0, 0, 0.04)',
+                    selected: 'rgba(0, 0, 0, 0.08)',
+                    disabled: 'rgba(0, 0, 0, 0.26)',
+                    disabledBackground: 'rgba(0, 0, 0, 0.12)',
+                },
+            }
+            : {
+                // palette values for dark mode
+                divider: 'rgba(255, 255, 255, 0.12)',
+                background: {
+                    default: '#121212',
+                    paper: '#121212',
+                },
+                text: {
+                    primary: '#fff',
+                    secondary: 'rgba(255, 255, 255, 0.7)',
+                    disabled: 'rgba(255, 255, 255, 0.5)',
+                },
+                action: {
+                    active: '#fff',
+                    hover: 'rgba(255, 255, 255, 0.08)',
+                    selected: 'rgba(255, 255, 255, 0.16)',
+                    disabled: 'rgba(255, 255, 255, 0.3)',
+                    disabledBackground: 'rgba(255, 255, 255, 0.12)',
+                },
+            }),
+    },
+});
